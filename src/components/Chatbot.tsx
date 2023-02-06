9480561660949d1b675f9b956f7849b99798ffb6
Jan li <bingox29@gmail.com>
2023-02-05 22:51:59 -0500

    styled chatbot

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -1,37 +1,58 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './Chatbot.css'
+import React, { useState } from "react";
+import axios from "axios";
+import "./Chatbot.css";
 
-
-interface ChatbotProps { }
+interface ChatbotProps {}
 
 const Chatbot: React.FC<ChatbotProps> = () => {
-  const [input, setInput] = useState('');
-  const [response, setResponse] = useState('');
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   try {
+  //     const { data } = await axios.post("http://localhost:3300/users/chat", {
+  //       input,
+  //     });
+  //     console.log(data);
+  //     setResponse(data.response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  const postMessage = (input: string) => {
+    axios
+      .post("http://localhost:3300/users/chat", { input })
+      .then((response) => {
+        console.log(response);
+        setResponse(response.data);
+      });
+  };
+  //  .catch((error)=> {
+  //       console.error(error);
+  //     });
+  //   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      const { data } = await axios.post('http://localhost:3300/users/chat', { input });
-      setResponse(data.response);
-    } catch (error) {
-      console.error(error);
-    }
+    postMessage(input);
   };
 
   return (
     <div className="chatbot">
-      <div className="chatbot-header">
-        Chatbot
-      </div>
+      <div className="chatbot-header">Chatbot</div>
+      <div className="chatbot-response">{response}</div>
       <div className="chatbot-body">
         <form className="chatbot-form" onSubmit={handleSubmit}>
-          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Ask something..." />
+          <textarea
+            // type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Ask something..."
+          />
           <button type="submit">Send</button>
         </form>
-        <div className="chatbot-response">
-          {response}
-        </div>
       </div>
     </div>
   );
